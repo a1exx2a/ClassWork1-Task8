edc21600bd0830761840292f85dc069a9e95f8ea
a1exx2a <a1exx2a@yandex.ru>
2024-12-08 17:50:53 +0300

    init commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task8;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] x = {10,2,3,4,5};
+
+        int y1 = x[0];
+        foreach (int i in x) {
+            if (i > y1) {
+                y1 = i;
+            }
+        
+        }
+
+        int y2 = x[0];
+        foreach (int i in x){
+            if (i < y2){
+                y2 = i;
+            }
+        }
+
+
+        Console.WriteLine($"Максимальное значение в массиве --> {y1}");
+        Console.WriteLine($"Минимальное значение в массиве --> {y2}");
+    }
+}
